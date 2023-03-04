86bcb31092ea2ade80e36fe6d15ae843df3302dc
poojakhandale <poojakhandale2000@gmail.com>
2023-03-04 23:45:09 +0530

    Reflection

---
+++ MoodAnalyserFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day21Reflection
+{
+   
+        public class MoodAnalyzerFactory
+        {
+            public static AnalyzeMood CreateMoodAnalyzer()
+            {
+                try
+                {
+                    Type type = Type.GetType("Day21Reflections.AnalyzeMood");
+                    AnalyzeMood moodAnalyzer = (AnalyzeMood)Activator.CreateInstance(type);
+                    return moodAnalyzer;
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new MoodAnalysisException("Exception Raised: Class not found!");
+                }
+                catch (Exception)
+                {
+                    throw new MoodAnalysisException("Exception Raised: Constructor is not found!");
+                }
+            }
+        }
+    }
+
+
