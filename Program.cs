6098cd5ca1849969af9d726e022ef1d68f2e027a
poojakhandale <poojakhandale2000@gmail.com>
2023-03-04 23:46:18 +0530

    Using reflection for mood Analyzer

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day21Reflection;
+
+namespace Day21Reflections
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                AnalyzeMood mood = MoodAnalyzerFactory.CreateMoodAnalyzer();
+                Console.WriteLine(mood.Analyze(""));
+            }
+            catch (MoodAnalysisException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
